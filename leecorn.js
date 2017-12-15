7d0a3d3b23305111fd55504627781daabcce62a4
sheita <clemgoupil@me.com>
2017-12-15 21:21:26 +0100

    Improved code structure & lots of small improvements

--- leecorn.js
+++ leecorn.js
@@ -1,31 +1,114 @@
 // Déclaration des variables
 
+// Variable principale du jeu
+var game = {
+  state: 0,
+  time: 0,
+  numberOfPipes: 0,
+  speed: 6,
+  holeHeight: 200,
+  score: 0,
+  scoreStep: 1,
+  timeWhenLost: 0,
+  canClickAfterDeath: false,
+  init: function() {
+    this.time = 0;
+    this.numberOfPipes = 0;
+    licorne.x = width/5;
+    licorne.y = height/2;
+    this.score = 0;
+    this.scoreStep = 1;
+    this.holeHeight = 200;
+    this.fullTime = 0;
+    this.canClickAfterDeath = false;
+  },
+  fullTime: 0,
+  decreasingHoleHeight: function() {
+    if(game.fullTime % 90 == 0) {
+      game.holeHeight -= 1;
+    }
+  } 
+}
+
+// Variable qui caractérise flappy leecorn
+var licorne = {
+  x: 0,
+  y: 0,
+  taille: 34,
+  afficher: function() {
+    image(flap, licorne.x, licorne.y, licorne.taille, licorne.taille);
+  },
+  yLastTap: 0
+}
+
+// Variable qui vérifie le score et si le joueur a perdu ou non
+var check = {
+  score: function() {
+    switch(game.scoreStep) {
+    case 1:
+      if(licorne.x > pipe1.x) {game.score += 1; game.scoreStep = 2;}
+      break;
+    case 2:
+      if(licorne.x > pipe2.x) {game.score += 1; game.scoreStep = 3;}
+      break;
+    case 3:
+      if(licorne.x > pipe3.x) {game.score += 1; game.scoreStep = 1;}
+      break;
+    }
+  },
+  ifNotDead: function() {
+    if(licorne.x > pipe1.x-licorne.taille/2 && licorne.x < pipe1.x+50+licorne.taille/2) {
+      if((licorne.y-licorne.taille/2 < pipe1.hole - game.holeHeight/2) || (licorne.y+licorne.taille/2 > pipe1.hole + game.holeHeight/2)) {
+        game.state = 3;
+        game.timeWhenLost = game.fullTime;
+      }
+    }
+    if(licorne.x > pipe2.x-licorne.taille/2 && licorne.x < pipe2.x+50+licorne.taille/2) {
+      if((licorne.y-licorne.taille/2 < pipe2.hole - game.holeHeight/2) || (licorne.y+licorne.taille/2 > pipe2.hole + game.holeHeight/2)) {
+        game.state = 3;
+        game.timeWhenLost = game.fullTime;
+      }
+    }
+    if(licorne.x > pipe3.x-licorne.taille/2 && licorne.x < pipe3.x+50+licorne.taille/2) {
+      if((licorne.y-licorne.taille/2 < pipe3.hole - game.holeHeight/2) || (licorne.y+licorne.taille/2 > pipe3.hole + game.holeHeight/2)) {
+        game.state = 3;
+        game.timeWhenLost = game.fullTime;
+      }
+    }
+  }, 
+  ifPipeAway: function() {
+    if(pipe1.x < -200) { 
+      pipe1.x = 1600;
+      pipe1.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+    }
+    if(pipe2.x < -200) { 
+      pipe2.x = 1600;
+      pipe2.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+    }
+    if(pipe3.x < -200) {
+      pipe3.x = 1600;
+      pipe3.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+    }
+  }
+}
+
+function Pipe() {
+  game.numberOfPipes += 1;
+  this.x = 500 + 600 * game.numberOfPipes;
+  this.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+  
+  this.afficherPipe = function() {
+    fill(0);
+    rect(this.x,0,50,this.hole-game.holeHeight/2);
+    rect(this.x,this.hole+game.holeHeight/2,50,height-this.hole-game.holeHeight/2);
+    fill(255);
+  }
+}
+
 // Images
 var clouds, flap, intro, perdu;
-// Etape initial du jeu
-// 0 - Accueil  1 - Prêt à démarrer  2 - Jeu  3 - Fin du jeu
-var gameState = 0;
-// Variable de temps
-var t = 0;
-// Variables de position de la licorne
-var xLicorne, yLicorne;
-// Taille de flap
-var tailleFlap = 30;
-// Score
-var score = 0;
-var scoreStep = 1;
-// Variables pipes
-var pipes = 0;
-// Vitesse des pipes
-var vitesse = 6;
-// Hauteur du trou des pipes
-var holeHeightOriginal = 200;
-var holeHeight = holeHeightOriginal;
-
-var timeDraw = 0;
 
 // Préchargement
-
 function preload() {
   clouds = loadImage('images/clouds.jpg');
   flap = loadImage('images/flapflap.png');
@@ -33,8 +116,7 @@ function preload() {
   perdu = loadImage('images/perdu.png');
 }
 
-// Affichage du programme
-
+// Premier affichage du programme
 function setup() {
   createCanvas(800,500); // Affichage de la fenêtre
   imageMode(CENTER); // Centrage des images
@@ -45,9 +127,7 @@ function setup() {
   textStyle(BOLD);
   fill(255);
 
-  // Définition de la position de la licorne par rapport à l'écran
-  xLicorne = width/5;
-  yLicorne = height/2;
+  game.init();
 
   // Pipes
   pipe1 = new Pipe();
@@ -56,26 +136,25 @@ function setup() {
 }
 
 // Fonction répétée 30 fois par secondes
-
 function draw() {
   image(clouds, width/2, height/2, width, height); // Fond d'écran nuages
-  t+=1; // Temps à 30 images/secondes
+  game.time+=1; // Temps à 30 images/secondes
   
 
-  switch(gameState) {
+  switch(game.state) {
   case 0: // Acceuil du jeu
     image(intro, width/2, height/2, width, height);
     break;
 
   case 1: // Prêt à démarrer
-    yLicorne = yLicorne + Math.cos(t/30);
+    licorne.y = licorne.y + Math.cos(game.time/30);
     licorne.afficher();
     break;
 
   case 2: // Jeu
-    yLicorne = licorne.x(yLicorneTap);
-    if (yLicorne>height+tailleFlap || yLicorne<0-tailleFlap) { 
-      gameState = 3;
+    licorne.y = (1/4)*game.time*game.time-10*game.time+licorne.yLastTap;
+    if (licorne.y>height+licorne.taille || licorne.y<0-licorne.taille) { 
+      game.state = 3;
     }
     licorne.afficher();
      
@@ -83,140 +162,69 @@ function draw() {
     pipe2.afficherPipe();
     pipe3.afficherPipe();
     
-    checkScore();
+    check.score();
     textSize(50);
-    text(score, 95*width/100, 60);
+    text(game.score, 95*width/100, 60);
     
-    timeDraw += 1;
-    if(timeDraw % 40 == 0) {
-      holeHeight -= 1;
-    }
+    pipe1.x -= game.speed;
+    pipe2.x -= game.speed;
+    pipe3.x -= game.speed;
     
-    pipe1.x -= vitesse;
-    pipe2.x -= vitesse;
-    pipe3.x -= vitesse;
+    check.ifNotDead();
+    check.ifPipeAway();
     
-    if(xLicorne > pipe1.x && xLicorne < pipe1.x+50) {
-      if((yLicorne < pipe1.hole - holeHeight/2) || (yLicorne > pipe1.hole + holeHeight/2)) {
-        gameState = 3;
-      }
-    }
-    if(xLicorne > pipe2.x && xLicorne < pipe2.x+50) {
-      if((yLicorne < pipe2.hole - holeHeight/2) || (yLicorne > pipe2.hole + holeHeight/2)) {
-        gameState = 3;
-      }
-    }
-    if(xLicorne > pipe3.x && xLicorne < pipe3.x+50) {
-      if((yLicorne < pipe3.hole - holeHeight/2) || (yLicorne > pipe3.hole + holeHeight/2)) {
-        gameState = 3;
-      }
-    }
-    
-    if(pipe1.x < -200) { 
-      pipe1.x = 1600;
-      pipe1.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-    }
-    if(pipe2.x < -200) { 
-      pipe2.x = 1600;
-      pipe2.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-    }
-    if(pipe3.x < -200) {
-      pipe3.x = 1600;
-      pipe3.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-    }
+    game.fullTime += 1;
+    game.decreasingHoleHeight();
     break;
 
   case 3: // Perdu & Score
+    game.fullTime += 1;
     image(perdu, width/2, height/3, width, height);
     textSize(20);
-    text('SCORE : ' + score, width/2, (15*height)/28);
+    text('SCORE : ' + game.score, width/2, (15*height)/28);
     break;
   }
 }
 
-// Fonction qui s'active quand l'écran d'un smartphone est pressé
-
-function touchStarted() {
-  switch(gameState) {
+// Fonction qui s'active quand on appuie sur une touche
+function keyPressed() {
+  switch(game.state) {
   case 0: // Acceuil du jeu
-    gameState = 1;
+    game.state = 1;
     break;
 
   case 1: // Prêt à démarrer
-    gameState = 2;
-    yLicorneTap = yLicorne; 
-    t = 0;
+    game.state = 2;
+    licorne.yLastTap = licorne.y; 
+    game.time = 0;
     break;
 
   case 2: // Jeu
-    gameState = 2;
-    yLicorneTap = yLicorne;
-    t = 0;
+    game.state = 2;
+    licorne.yLastTap = licorne.y; 
+    game.time = 0;
     break;
 
   case 3: // Perdu & Score
-    score = 0;
-    gameState = 1;
-    xLicorne = width/5;
-    yLicorne = height/2;
-    t=0;
-    timeDraw = 0;
-    holeHeight = holeHeightOriginal;
-    
-    pipes = 1;
-    pipe1.x = 200 + 600 * pipes;
-    pipes+=1
-    pipe2.x = 200 + 600 * pipes;
-    pipes+=1
-    pipe3.x = 200 + 600 * pipes;
-    pipe1.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-    pipe2.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-    pipe3.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-    break;
+    if(game.timeWhenLost+20 < game.fullTime) {
+      game.state = 1;
+      game.init();
+      
+      game.numberOfPipes = 1;
+      pipe1.x = 200 + 600 * game.numberOfPipes;
+      game.numberOfPipes += 1
+      pipe2.x = 200 + 600 * game.numberOfPipes;
+      game.numberOfPipes += 1
+      pipe3.x = 200 + 600 * game.numberOfPipes;
+      pipe1.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+      pipe2.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+      pipe3.hole = Math.random() * ((height - game.holeHeight/2) - (game.holeHeight/2 + 20)) + (game.holeHeight/2 + 20);
+      break;
+    }
   }
 }
 
-// Fonction qui s'active quand une touche est pressée sur ordinateur
-
-function keyPressed() { 
-  touchStarted();
-}
-
-// Variable de la licorne
-
-var licorne = {
-  x: function(yOrigin) {
-  return (1/4)*t*t-10*t+yOrigin;
-  // (1/2)*(9.8/30)*t*t-10*t+yOrigin;
-  }, 
-  afficher: function() {
-  image(flap, xLicorne, yLicorne, tailleFlap, tailleFlap);
-  }
-}
-
-function Pipe() {
-  pipes += 1;
-  this.x = 500 + 600 * pipes;
-  this.hole = Math.random() * ((height - holeHeight/2) - (holeHeight/2 + 20)) + (holeHeight/2 + 20);
-  
-  this.afficherPipe = function() {
-    fill(0);
-    rect(this.x,0,50,this.hole-holeHeight/2);
-    rect(this.x,this.hole+holeHeight/2,50,height-this.hole-holeHeight/2);
-    fill(255);
-  }
-}
-
-function checkScore() {
-  switch(scoreStep) {
-    case 1:
-    if(xLicorne > pipe1.x) {score += 1; scoreStep = 2;}
-    break;
-    case 2:
-    if(xLicorne > pipe2.x) {score += 1; scoreStep = 3;}
-    break;
-    case 3:
-    if(xLicorne > pipe3.x) {score += 1; scoreStep = 1;}
-    break;
-  }
+// Compatibilité smartphones et clic de souris
+function touchStarted() { 
+  keyPressed();
 }
